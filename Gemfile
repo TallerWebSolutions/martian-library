92de695aa322f6eba7ec3b8285431ded83d85991
Celso Martins <celso.av.martins@gmail.com>
2021-12-05 11:43:13 -0300

    rails INIT

---
+++ Gemfile
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.1'
+
+gem 'jbuilder', '~> 2.7'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'sass-rails', '>= 6'
+gem 'webpacker', '~> 5.0'
+
+gem 'bootsnap', '>= 1.4.4', require: false
+
+group :test do
+  gem 'rspec-rails'
+  gem 'rubocop'
+  gem 'rubocop-performance'
+  gem 'rubocop-rails'
+  gem 'rubocop-rspec'
+  gem 'shoulda-matchers'
+end
+
+group :development do
+  gem 'listen', '~> 3.3'
+  gem 'rack-mini-profiler', '~> 2.0'
+  gem 'web-console', '>= 4.1.0'
+end
+
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'rspec/collection_matchers'
+
+Dir[Rails.root.join('spec/support/*.rb')].each { |f| require f }
+
+Rails.logger.level = 4
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.order = :random
+  config.profile_examples = 10
+  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include Warden::Test::Helpers
+  Warden.test_mode!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:transaction)
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  config.include ActiveJob::TestHelper
+  config.after do
+    clear_enqueued_jobs
+  end
+
+  config.after(:all) do
+    FileUtils.rm_rf(Dir[Rails.root.join('public/uploads/tmp/*')]) if Rails.env.test?
+  end
+end
